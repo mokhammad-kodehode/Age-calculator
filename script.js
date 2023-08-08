892a5c332333092c9609c7fcdb7305e42b94a09b
MokhammadVu <stechkin_95@hotmail.com>
2023-08-08 08:59:14 +0200

    Update

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-// Your challenge is to build out this age calculator app
-//  and get it looking as close to the design as possible.
-
-// You can use any tools you like to help you complete the
-//  challenge. So if you've got something you'd like to practice,
-//   feel free to give it a go.
-
-// View an age in years, months,
-//  and days after submitting a valid date through the form
-// Receive validation errors if:
-// Any field is empty when the form is submitted
-// The day number is not between 1-31
-// The month number is not between 1-12
-// The year is in the future
-// The date is invalid, e.g. 31/04/1991 (there are 30 days in April)
-// View the optimal layout for the interface
-// depending on their device's screen size
-// See hover and focus states for all interactive elements
-// on the page
-
-// Bonus: See the age numbers animate to their final number
-//  when the form is submitted
-// Download the project and go through the README.md file.
-//  This will provide further details about the project and help
-//  you get set up.
+// Create variable to the input fields and add event listeners to them
 
 const inputYear = document.getElementById("year");
 const inputDay = document.getElementById("day");
@@ -32,15 +8,25 @@ inputYear.addEventListener("input", update);
 inputDay.addEventListener("input", update);
 inputMonth.addEventListener("input", update);
 
+// Function to calculate and update the countdown between the selected date and today.
+
 function update() {
-  let month = inputMonth.value - 1;
+  // Get the values from the input fields
+
+  let month = inputMonth.value - 1; // Months in JavaScript's Date object are 0 index
   let day = inputDay.value;
   let year = inputYear.value;
+
+  // Create Date objects for today's date
 
   const today = new Date();
   const selectedDate = new Date(year, month, day);
 
+  // Calculate the time difference between today and the selected date
+
   const callDate = today.getTime() - selectedDate.getTime();
+
+  // Calculate the years, months, and days difference
 
   let years = Math.floor(callDate / (1000 * 60 * 60 * 24 * 365.25));
   let months = Math.floor(
@@ -50,9 +36,13 @@ function update() {
     (callDate % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24)
   );
 
+  // Update the displayed countdown values
+
   document.getElementById("countd").textContent = days;
   document.getElementById("countm").textContent = months;
   document.getElementById("county").textContent = years;
+
+  // Add "days", "months", and "years" labels to the displayed countdown values
 
   const daySpan = document.createElement("span");
   daySpan.textContent = "days";
@@ -65,16 +55,22 @@ function update() {
   const yearSpan = document.createElement("span");
   yearSpan.textContent = "years";
   document.getElementById("county").append(yearSpan);
+
+  const error = ageError();
+
+  // Обработка ошибок
+  if (error) {
+    // Делайте здесь что-то с сообщением об ошибке, например, выводите на экран
+    console.error(error);
+  }
 }
 
-// function checkAge() {}
-
-// function ageError() {
-//   if (day.value < 1 || day > 31) {
-//     return "Must be a valid day";
-//   } else if (month.value < 1 || month > 12) {
-//     return "Must be a valid month";
-//   } else if (year.value < 1900 || year > 2023) {
-//     return "Must be a valid year";
-//   }
-// }
+function ageError() {
+  if (inputDay.value < 1 || inputDay.value > 31) {
+    return "Must be a valid day";
+  } else if (inputMonth.value < 1 || inputMonth.value > 12) {
+    return "Must be a valid month";
+  } else if (inputYear.value < 1900 || inputYear.value > 2023) {
+    return "Must be a valid year";
+  }
+}
